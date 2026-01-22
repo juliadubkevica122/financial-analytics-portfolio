b49cc867d9e080e45f7de2338acdcf61e94becb5
juliadubkevica122 <juliadubkevica@gmail.com>
2026-01-22 11:15:42 +0000

    Update risk_rules.sql

--- sql/risk_rules.sql
+++ sql/risk_rules.sql
@@ -1,1 +1,19 @@
+-- Compliance / Risk Analytics
+-- Rule-based risk flags for transactions
 
+SELECT
+  transaction_id,
+  transaction_date,
+  amount,
+  category,
+  country,
+  CASE
+    WHEN amount > 10000 AND category = 'crypto'
+      THEN 'High risk: large crypto'
+    WHEN category = 'casino'
+      THEN 'High risk: gambling'
+    WHEN country <> 'IE'
+      THEN 'Medium risk: foreign country'
+    ELSE 'Low risk'
+  END AS risk_flag
+FROM transactions;
